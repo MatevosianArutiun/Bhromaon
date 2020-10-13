a41f19085b5af6cb720e2bc4e0a224ff46affbc2
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-10-13 03:04:18 +0300

    deploy

---
+++ js/main.js
@@ -0,0 +1,3 @@
+!function(n){"function"==typeof define&&define.amd?define("main",n):n()}((function(){}));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
